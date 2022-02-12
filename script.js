8f9e3941cc22efec448c78c36287dbb9f5f7c5d3
hugomatton <hugomattonisis@gmail.com>
2022-02-12 22:28:36 +0100

    Ajout de qq pion + fonction deplacer pion

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+
+
+let deplacerPion = function(dep, arr){
+    let image_dep = document.querySelector(`#${dep} img`).src
+    document.querySelector(`#${dep} img`).src = ""
+    document.querySelector(`#${arr} img`).src = image_dep
+}
+
+deplacerPion("H1", "H2")
+deplacerPion("G2","F2")
